8e53cca2f26d5ffb085f57dfef66e871387c01d4
André Kraüss <andre.kmbv@gmail.com>
2018-11-15 01:34:34 -0400

    Added css styles

--- public/javascripts/channelsT.js
+++ public/javascripts/channelsT.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
                     id: id,
                 }
 
-                $("#main-content").append(template(context));
+                $("#main-content-channels").append(template(context));
 
                 item.videos.forEach(function (url) {
                     var context2 = {
