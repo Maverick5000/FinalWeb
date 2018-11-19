8e875b12a0a34b50aa4feefa080abdf950353915
André Kraüss <andre.kmbv@gmail.com>
2018-11-18 20:20:45 -0400

    Added add video to channel

--- API/routes/addvideos.js
+++ API/routes/addvideos.js
@@ -2,20 +2,34 @@ var express = require('express');
 var router = express.Router();
 const Channel = require('../models/channel');
 var mongoose = require('mongoose');
+const Video = require('../models/video');
+var vidId;
+var channel;
 
 router.post('/:channelId', function (req, res, next) {
   if (!req.user) {
     res.render('login');
   } else {
-    
-    Channel.findByIdAndUpdate({ _id: req.params.channelId }, { $push: { videos: req.body.url }}, { new: true })
-    //Channel.updateOne({_id: req.params.id}, {$push: {videos: req.body.url}}, { new: true })
+    const video = new Video({
+      _id: new mongoose.Types.ObjectId(),
+      srcUrl: req.body.url,
+      nombre: req.body.nombreVid,
+      user: req.user._id,
+      descripcion: req.body.descripcion,
+      usernombre: req.user.username
+  });
+  video.save().then(result => {
+    vidId = result._id;
+    channel = req.params.channelId;
+    console.log("PRINT VIDEO ID "+vidId);
+    console.log("PRINT CHANNEL ID "+channel);
+    Channel.findByIdAndUpdate( {_id: channel} , {$push: {videos:vidId}}, { new: true })
     .exec()
     .then(docs => {
       console.log(docs);
       res.render('mychannels', {
         title: 'Express',
-        username: req.user.username,
+        username: req.user.username
     });
     })
     .catch(err => {
@@ -24,6 +38,7 @@ router.post('/:channelId', function (req, res, next) {
         error: err
       });
     });
+  });
   }
 });
 
